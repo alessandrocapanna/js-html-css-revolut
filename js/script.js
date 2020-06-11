e6b955a029aa2b136d417146668ff00466ee620e
alessandrocapanna <acapanna75@gmail.com>
2020-06-11 17:04:22 +0200

    commit

--- js/script.js
+++ js/script.js
@@ -7,11 +7,18 @@ $(document).ready(
         $(this).children('.drop').addClass('active');
       }
     );
+    // NOTE: quando clicco il with drop esce il drop
     $('.with-drop > a').click(
       function(){
         $(this).siblings('.drop').toggleClass('active');
       }
     );
+    // // NOTE: quando clicco fuori il drop il drop scompare ma non funziona :C
+    // $(document).click(
+    //   function(){
+    //     $('.drop.active').removeClass('active');
+    //   }
+    // );
 
     // NOTE: per il cambio icona
     $('.it').mouseenter(
@@ -24,6 +31,22 @@ $(document).ready(
       function(){
         $('.it i').removeClass('fa-sort-up');
         $('.it i').addClass('fa-sort-down');
+      }
+    );
+
+    // NOTE: per click su burger
+    $('.burger > i:first-child').click(
+      function(){
+        $('.burger i:first-child').addClass('hidden');
+        $('.burger ul').show();
+        $('.burger i:nth-child(2)').removeClass('hidden');
+      }
+    );
+    $('.burger > i:nth-child(2)').click(
+      function(){
+        $('.burger i:nth-child(2)').addClass('hidden');
+        $('.burger ul').hide();
+        $('.burger i:first-child').removeClass('hidden');
       }
     );
   }
