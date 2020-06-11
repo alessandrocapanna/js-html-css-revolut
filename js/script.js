cb84398c8162a7c8709e7d30ddf47002d875f241
alessandrocapanna <acapanna75@gmail.com>
2020-06-11 11:10:33 +0200

    commit

--- js/script.js
+++ js/script.js
@@ -1,26 +1,30 @@
-// NOTE: per il dropdown
-$('.with-drop').mouseenter(
+$(document).ready(
   function(){
-    $('.drop').removeClass('active');
-    $(this).children('.drop').addClass('active');
-  }
-);
-$('.with-drop').click(
-  function(){
-    $(this).children('.drop').removeClass('active');
-  }
-);
+    // NOTE: per il dropdown
+    $('.with-drop').mouseenter(
+      function(){
+        $('.drop').removeClass('active');
+        $(this).children('.drop').addClass('active');
+      }
+    );
+    $('.with-drop > a').click(
+      function(){
+        $(this).siblings('.drop').toggleClass('active');
+      }
+    );
 
-// NOTE: per il cambio icona
-$('.it').mouseenter(
-  function(){
-    $('.it i').removeClass('fa-sort-down');
-    $('.it i').addClass('fa-sort-up');
-  }
-);
-$('.it').mouseleave(
-  function(){
-    $('.it i').removeClass('fa-sort-up');
-    $('.it i').addClass('fa-sort-down');
+    // NOTE: per il cambio icona
+    $('.it').mouseenter(
+      function(){
+        $('.it i').removeClass('fa-sort-down');
+        $('.it i').addClass('fa-sort-up');
+      }
+    );
+    $('.it').mouseleave(
+      function(){
+        $('.it i').removeClass('fa-sort-up');
+        $('.it i').addClass('fa-sort-down');
+      }
+    );
   }
 );
